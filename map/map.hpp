ebb603826bd1a42510898f2fecbfb690911b16a4
zero <mohammdymehdi@gmail.com>
2023-02-04 00:14:57 +0100

    define member variabls

--- map/map.hpp
+++ map/map.hpp
@@ -4,9 +4,33 @@
 
 namespace ft
 {
-    template < class Key, class T,class Compare = less<Key>,  class Alloc = allocator<pair<const Key,T> > >
+    template <class Key, class T, class Compare = less<Key>, class Alloc = allocator<pair<const Key, T>>>
     class map
     {
+    public:
+        typedef Key key_type;
+        typedef T mapped_type;
+        typedef pair<const Key, T> value_type;
+        typedef Compare key_compare;
+        typedef Alloc allocator_type;
+        typedef value_type &reference;
+        typedef const value_type &const_reference;
+        typedef value_type *pointer;
+        typedef const value_type *const_pointer;
+
+    private:
+        struct Node
+        {
+            value_type value;
+            Node *left, *right;
+            Node(const value_type &v) : value(v), left(nullptr), right(nullptr) {}
+        };
+
+        Node *root;
+        allocator_type alloc;
+        key_compare cmp;
+        size_t size 
+    public:
         
     }
 }
